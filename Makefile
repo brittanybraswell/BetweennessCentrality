51a8d9790efe615f716ed16043ecdd77e1d37e01
Kyle Laker <kyle@laker.email>
2018-04-10 15:32:39 -0400

    Add degree centrality as example
    Hopefully looking at a really simple centrality example will give us
    some insight to how igraph works as well as where to go from here.

--- Makefile
+++ Makefile
@@ -3,21 +3,24 @@ CC = gcc
 CLUSTER_LDFLAG=-L/shared/lib/igraph-0.7.1/build/lib
 CLUSTER_CFLAG=-I/shared/lib/igraph-0.7.1/build/include/igraph
 
-override CFLAGS := -O2 -fopenmp -ligraph $(CLUSTER_CFLAG) $(CFLAGS)
+override CFLAGS := -std=c99 -O2 -fopenmp -ligraph $(CLUSTER_CFLAG) $(CFLAGS)
 override LDFLAGS := -fopenmp -ligraph $(CLUSTER_LDFLAG) $(LDFLAGS)
 
 SRCDIR=bc
-SRCS=graph.c vertex_bt.c edge_bt.c
+SRCS=graph.c vertex_bt.c edge_bt.c degree.c
 OBJS=$(SRCS:.c=.o)
 
 default: all
 
-all: edge_bt vertex_bt
+all: edge_bt vertex_bt degree
 
 edge_bt: graph.o edge_bt.o
 	$(CC) $(LDFLAGS) $^ -o $@
 
 vertex_bt: graph.o vertex_bt.o
+	$(CC) $(LDFLAGS) $^ -o $@
+
+degree: graph.o degree.o
 	$(CC) $(LDFLAGS) $^ -o $@
 
 %.o: $(SRCDIR)/%.c
---
+++ bc/degree.c
@@ -0,0 +1,73 @@
+#include <stdbool.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+#include "graph.h"
+#include "util.h"
+
+/*
+ * This is not actually an example of betweenness centrality. It is
+ * degree centrality. It is just in this folder to make things easier for us.
+ */
+
+struct {
+    double score;
+    int id;
+} winner;
+
+double get_degree(igraph_t*, int);
+
+int main(int argc, char *argv[]) {
+    igraph_t graph = create_graph(argv[1]); // creates graph of given dataset
+
+    int vcount = igraph_vcount(&graph); // number of edges in graph
+
+    START_TIMER(find_max)
+    /*
+     * This doesn't actually scale particularly well. In fact, it appears
+     * that in most all cases, the OMP version is significantly worse than
+     * the original serial version and then using any OMP_THREAD_COUNT
+     * results in approximately the same time regardless of the number of
+     * threads.
+     *
+     * This actually makes some sense. Degree centrality is a very local
+     * view of the graph; you're simply seeing how many edges point to each
+     * vertex, and each thread gets assigned particular vertices to check.
+     * This is a relatively fast operation and without an absolutely
+     * enormous number of nodes, the speedup from parallelizing is not
+     * sufficient to overcome the penalties of locking/unlocking for the
+     * critical section.
+     */
+#   pragma omp parallel for default(none) shared(winner, graph, vcount)
+    for (int vertex = 0; vertex < vcount; vertex++) {
+        double result = get_degree(&graph, vertex);
+#       pragma omp critical
+        if (result > winner.score) {
+            winner.score = result;
+            winner.id = vertex;
+        }
+    }
+    STOP_TIMER(find_max);
+
+    printf("Vertex: %d\t", winner.id);
+    printf("Time: %f\n", GET_TIMER(find_max));
+
+    return 0;
+}
+
+double get_degree(igraph_t *graph, int vertex_id) {
+    igraph_vector_t degree;
+    igraph_vs_t vs = igraph_vss_1(vertex_id);
+
+    igraph_vector_init(&degree, 1);
+
+    igraph_degree(graph, &degree, vs, IGRAPH_ALL, true);
+
+    int result = VECTOR(degree)[0];
+
+    igraph_vector_destroy(&degree);
+
+    return result;
+}
