89dcd7528ea205be1d5ef4cd2d51eb0d2ef7a7df
20180105 <hawkinjc@login01.cluster.cs.jmu.edu>
2018-04-17 00:34:47 -0400

    Updated the comment explainations

--- bc/girvan-newman.c
+++ bc/girvan-newman.c
@@ -26,27 +26,40 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    igraph_real_t max;
-    igraph_es_t edge_selector;
-    int ret;
+    igraph_real_t max;                       // the edge id of the edge with highest ebt
+    igraph_es_t edge_selector;               // an iterator for to select the edge to remove
+    int ret;                                 // used to check that igraph functions ran correctly
 
-    graph = create_graph(argv[1]); // creates graph of given dataset
-    ecount = igraph_ecount(&graph); // number of edges in graph
+    graph = create_graph(argv[1]);           // creates graph of given dataset
+    ecount = igraph_ecount(&graph);          // number of edges in graph
 
+    // print the number of edges before removal
     fprintf(stderr,"Number of edges is %d\n", ecount);
 
+    // calculate the time to find and remove the edge with the highest ebt
     START_TIMER(find_max)
 
+    // use the function that uses igraphs edge betweenness function to get an edge id
     max = find_max_betweenness();
+
+    // make an edge selector for just the edge with the selected edge id
     igraph_es_1(&edge_selector, max);
+
+    // delete the edge from the graph
     ret = igraph_delete_edges(&graph, edge_selector);
     STOP_TIMER(find_max);
 
+    // determine the number of edges after the deletion
     ecount = igraph_ecount(&graph);
+
+    // print the new number of edges to verify only one was removed
     fprintf(stderr,"Number of edges is %d\n", ecount);
 
+    // clean up the memory used
     igraph_es_destroy(&edge_selector);
     igraph_destroy(&graph);
+
+    // print the run time
     fprintf(stderr,"Time: %f\n", GET_TIMER(find_max));
 
     return EXIT_SUCCESS;
@@ -58,23 +71,28 @@ int main(int argc, char *argv[]) {
  * the igraph_edge_betweenness() function.
  */
 igraph_real_t find_max_betweenness() {
-    igraph_real_t max_edge;
-    igraph_vector_t edges;
+    igraph_real_t max_edge;          // the id of the edge with the highest ebt
+    igraph_vector_t edges;           // this vector will store the edge betweenness values for each edge id
 
+    // create a vector to hold all of the edge betweenness values
     igraph_vector_init(&edges, ecount);
 
+    // fill the vector with the ebt values
     igraph_edge_betweenness(&graph, &edges, IGRAPH_UNDIRECTED, 0);
 
+    // get the edge id of the edge with the greatest value
     max_edge = igraph_vector_max(&edges);
 
+    // print the edge id of the selected edge
     fprintf(stderr, "Edge Selected: %li\n", max_edge);
 
-    fprintf(stderr, "Edge Selected: %li\n", max_edge);
-
+    // clean up the vector we used
     igraph_vector_destroy(&edges);
 
+    // return the id of the edge with the highest ebt value
     return max_edge;
 }
+
 /*
  * This function also finds the edge with the highest betweenness value
  * for a graph, which will be removed in main.  This version does not us
@@ -83,6 +101,7 @@ igraph_real_t find_max_betweenness() {
  * the edge betweenness values are being calculated.
  */
 igraph_real_t max_betweenness() {
+    // just a temp value for compiling the stub
     igraph_real_t max_edge = 0;
 
 
